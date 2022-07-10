610d678e989c4f59fffff27eb378cf611301b0b7
experiencei <ayelojahighbee01@gmail.com>
2022-07-10 20:16:46 +0100

    added sidebar , frontview and appropiate icons

---
+++ partnershe/pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+
+const Home = () => {
+
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify - Dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h2>hELLO WORLD</h2>
+    </div>
+  );
+
+}
+
+export default Home;
